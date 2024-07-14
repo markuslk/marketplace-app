3f7b7baf32dc5612949f22c27918759726910cee
Markus <markuslokk@gmail.com>
2024-07-14 21:19:20 +0300

    add toaster

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/sonner";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
@@ -10,6 +11,7 @@ export const Route = createRootRoute({
 			</div>
 			<hr />
 			<Outlet />
+			<Toaster />
 		</>
 	),
 });
