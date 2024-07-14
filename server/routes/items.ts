049acdc788e930f717a924d2b2adf178890dc630
Markus <markuslokk@gmail.com>
2024-07-14 15:23:17 +0300

    refactor items route

--- server/routes/items.ts
+++ server/routes/items.ts
@@ -1,9 +1,28 @@
 import { Hono } from "hono";
 import { db } from "../db";
-import { items as itemsTable } from "../db/schema/items";
+import { itemsTable } from "../db/schema/schema";
+import type { Context } from "../lib/context";
 
-export const itemsRoute = new Hono().get("/", async (c) => {
-	const items = await db.select().from(itemsTable);
+export const itemsRoute = new Hono<Context>()
+	.get("/", async (c) => {
+		const items = await db.select().from(itemsTable);
 
-	return c.json({ items });
-});
+		return c.json({ items });
+	})
+	.post("/", async (c) => {
+		// const user = c.get("user");
+		// if (!user) {
+		// 	console.log("redirected");
+		// 	return c.redirect("/login");
+		// }
+
+		const result = await db
+			.insert(itemsTable)
+			.values({ id: "1", userId: "5", title: "some title" })
+			.returning()
+			.then((res) => res[0]);
+
+		c.status(201);
+		console.log(result);
+		return c.json(result);
+	});
