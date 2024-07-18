80f657cf65bb4f83c96c8addd7177b3835909f5c
Markus <markuslokk@gmail.com>
2024-07-18 14:24:11 +0300

    create middleware to check if there is session and/or user

--- server/middleware.ts
+++ server/middleware.ts
@@ -1,34 +1,41 @@
-import { Hono } from "hono";
 import { lucia } from "./lib/auth";
 import { getCookie } from "hono/cookie";
-import { csrf } from "hono/csrf";
+import { createMiddleware } from "hono/factory";
+import { type User, type Session } from "lucia";
 
-import type { User, Session } from "lucia";
-
-const app = new Hono<{
+type Env = {
 	Variables: {
 		user: User | null;
 		session: Session | null;
 	};
-}>();
+};
 
-app.use(csrf());
-
-app.use("*", async (c, next) => {
-	const sessionId = getCookie(c, lucia.sessionCookieName) ?? null;
-	if (!sessionId) {
-		c.set("user", null);
-		c.set("session", null);
-		return next();
+export const getUser = createMiddleware<Env>(async (c, next) => {
+	try {
+		const sessionId = getCookie(c, lucia.sessionCookieName) ?? null;
+		if (!sessionId) {
+			c.set("user", null);
+			c.set("session", null);
+			console.log("no session id middleware");
+			return next();
+		}
+		const { session, user } = await lucia.validateSession(sessionId);
+		if (session && session.fresh) {
+			c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
+				append: true,
+			});
+			console.log("session and session fresh middleware");
+		}
+		if (!session) {
+			c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
+				append: true,
+			});
+			console.log("no session middleware");
+		}
+		c.set("user", user);
+		c.set("session", session);
+		await next();
+	} catch (err) {
+		return c.json({ error: "Unauthorized" }, 401);
 	}
-	const { session, user } = await lucia.validateSession(sessionId);
-	if (session && session.fresh) {
-		c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
-			append: true,
-		});
-	}
-	c.set("user", user);
-	c.set("session", session);
-	console.log("middleware works");
-	return next();
 });
