d1ce7e3b54651c7f6fc5b1fb30f8aa0551a91de7
Markus <markuslokk@gmail.com>
2024-07-18 14:23:37 +0300

    create function for frontend api to get current user

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,21 @@
+import { type ApiRoutes } from "@server/app";
+import { queryOptions } from "@tanstack/react-query";
+import { hc } from "hono/client";
+
+const client = hc<ApiRoutes>("/");
+
+export const api = client.api;
+
+async function getCurrentUser() {
+	const res = await api.user.$get();
+	if (!res.ok) {
+		throw new Error("Server error");
+	}
+	const data = await res.json();
+	return data;
+}
+
+export const userQueryOptions = queryOptions({
+	queryKey: ["get-current-user"],
+	queryFn: getCurrentUser,
+});
