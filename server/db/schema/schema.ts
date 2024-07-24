79f38da7b54b93cf25a6cd01552704d8085d7dd3
Markus <markuslokk@gmail.com>
2024-07-24 15:35:25 +0300

    update db schema for items

--- server/db/schema/schema.ts
+++ server/db/schema/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, numeric, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const usersTable = pgTable("users", {
 	id: text("id").primaryKey(),
@@ -24,5 +26,21 @@ export const itemsTable = pgTable("items", {
 		.notNull()
 		.references(() => usersTable.id),
 	title: text("title").notNull(),
+	description: text("description").notNull(),
 	createdAt: timestamp("created_at").defaultNow(),
+	isPublished: boolean("is_published"),
+	startingPrice: numeric("starting_price", { precision: 12, scale: 2 }).notNull(),
+	currentOfferPrice: numeric("current_price", { precision: 12, scale: 2 }),
+	buyNowPrice: numeric("buy_price", { precision: 12, scale: 2 }).notNull(),
 });
+
+export const insertItemSchema = createInsertSchema(itemsTable, {
+	title: z.string().min(3, { message: "Title must be at least 3 characters" }),
+	description: z.string().min(60, { message: "Description must be at least 60 characters" }),
+	isPublished: z.boolean().default(false),
+	startingPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
+	currentOfferPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
+	buyNowPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
+});
+
+export const selectItemSchema = createSelectSchema(itemsTable);
