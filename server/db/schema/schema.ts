411b39064851bf907e88185036761d797084bb5f
Markus <markuslokk@gmail.com>
2024-08-20 16:50:56 +0300

    update schema with item timestamp values, change item id to serial, make buy now optional

--- server/db/schema/schema.ts
+++ server/db/schema/schema.ts
@@ -1,4 +1,5 @@
-import { boolean, numeric, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { boolean, numeric, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
@@ -21,25 +22,26 @@ export const sessionsTable = pgTable("sessions", {
 });
 
 export const itemsTable = pgTable("items", {
-	id: text("id").primaryKey(),
+	id: serial("id").primaryKey(),
 	userId: text("user_id")
 		.notNull()
 		.references(() => usersTable.id),
 	title: text("title").notNull(),
 	description: text("description").notNull(),
-	createdAt: timestamp("created_at").defaultNow(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	auctionEndsAt: timestamp("auction_ends_at")
+		.default(sql`(now() + interval '24 hours')`)
+		.notNull(),
 	isPublished: boolean("is_published"),
 	startingPrice: numeric("starting_price", { precision: 12, scale: 2 }).notNull(),
 	currentOfferPrice: numeric("current_price", { precision: 12, scale: 2 }),
-	buyNowPrice: numeric("buy_price", { precision: 12, scale: 2 }).notNull(),
+	buyNowPrice: numeric("buy_price", { precision: 12, scale: 2 }),
 });
 
 export const insertItemSchema = createInsertSchema(itemsTable, {
 	title: z.string().min(3, { message: "Title must be at least 3 characters" }),
 	description: z.string().min(60, { message: "Description must be at least 60 characters" }),
-	isPublished: z.boolean().default(false),
 	startingPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
-	currentOfferPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
 	buyNowPrice: z.string().regex(/^\d+(\.\d{1,2})?$/, { message: "Amount must be a valid monetary value" }),
 });
 
