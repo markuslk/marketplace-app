d501516c3197ff1176d51fa94327a1614333c3fb
Markus <markuslokk@gmail.com>
2024-08-20 18:27:44 +0300

    show create item option only if user is logged in

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,19 +5,25 @@ import { useQuery } from "@tanstack/react-query";
 import CreateItem from "@/components/CreateItem";
 
 export const Route = createFileRoute("/")({
+	loader: ({ context }) => {
+		const user = context.user;
+		if (!user) {
+			return { user: null };
+		}
+		return { user };
+	},
 	component: Component,
 });
 
 function Component() {
+	const { user } = Route.useRouteContext();
 	const { isPending, error, data: items } = useQuery(itemsQueryOptions);
 
 	if (error) return "An error has occured:" + error.message;
 
 	return (
 		<div className="px-4 py-6 lg:py-10">
-			<div>
-				<CreateItem />
-			</div>
+			{user ? <CreateItem /> : null}
 			<div className="text-2xl md:text-4xl font-semibold pb-4">Current offers:</div>
 			{isPending && <div>Loading items...</div>}
 			<div className="grid grid-cols-[repeat(auto-fill,minmax(260px,1fr))] xl:grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4 lg:gap-6 xl:gap-8">
